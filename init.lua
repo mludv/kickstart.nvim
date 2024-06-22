90c4f7e8bcbe779dd1ed05bf58f9ab89323d5701
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2024-06-22 16:00:04 +0800

    whitelist autoformat instead of blacklisting

--- init.lua
+++ init.lua
@@ -666,10 +666,12 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, python = true }
+        -- local disable_filetypes = { c = true, cpp = true, python = true }
+        local enabled_filetypes = { lua = true }
         return {
           timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          -- lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          lsp_fallback = enabled_filetypes[vim.bo[bufnr].filetype],
         }
       end,
       formatters_by_ft = {
